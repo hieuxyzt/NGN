9214e1cb926076f0d718c094cb76d1e169f36f1f
hieuxyzt <hieu650063@gmail.com>
2025-08-15 09:16:11 +0700

    add: Nginx HA

--- image_build.sh
+++ image_build.sh
@@ -1,7 +1,11 @@
-ROOT="/home/hieunn/containernet/examples/project"
-cd $ROOT/client_server
-docker build -t client -f Dockerfile.client .
-docker build -t server -f Dockerfile.server .
+ROOT="/home/hieunn/NGN"
 
-cd $ROOT/nginx
+cd $ROOT/client_server
+docker build -t server -f Dockerfile.server .
+docker build -t client -f Dockerfile.client .
+
+cd $ROOT/nginx/master
 docker build -t nginx -f Dockerfile.nginx .
+
+cd $ROOT/nginx/backup
+docker build -t nginx_backup -f Dockerfile.nginx .
