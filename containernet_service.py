dfb6a8015590f8febec716367f292d99c58647d2
hieuxyzt <hieu650063@gmail.com>
2025-08-15 10:07:31 +0700

    init: flask

---
+++ containernet_service.py
@@ -0,0 +1,76 @@
+from mininet.net import Containernet
+from mininet.node import Controller
+from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.log import info, setLogLevel
+
+
+def initContainernet():
+    setLogLevel('info')
+    net = Containernet(controller=Controller)
+    net.addController('c0')
+
+    info('*** Adding server and client container\n')
+    server = net.addDocker('server', ip='10.0.1.1', dcmd="python app.py", dimage="server:latest")
+    server2 = net.addDocker('server2', ip='10.0.1.2', dcmd="python app.py", dimage="server:latest")
+    server3 = net.addDocker('server3', ip='10.0.1.3', dcmd="python app.py", dimage="server:latest")
+
+    client = net.addDocker('client', ip='10.0.2.1', dimage="client:latest")
+    lb = net.addDocker(
+        'lb', ip='10.0.0.2', dimage="nginx:latest", 
+        publish_all_ports= True,
+        ports=[80], port_bindings={80:80}
+    )
+
+    lb_backup = net.addDocker(
+        'lb_backup', ip='10.0.0.3', dimage="nginx_backup:latest", 
+        #publish_all_ports= True,
+        #ports=[80], port_bindings={81:80}
+    )
+
+    info('*** Setup network\n')
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
+    s3 = net.addSwitch('s3')
+
+    net.addLink(server,s1)
+    net.addLink(server2,s1)
+    net.addLink(server3,s1)
+
+    net.addLink(lb,s2)
+    net.addLink(lb_backup,s2)
+
+    net.addLink(client,s3)
+
+    net.addLink(s1, s2, cls=TCLink, delay='10ms', bw=1)
+    net.addLink(s2, s3, cls=TCLink, delay='1ms', bw=1)
+    net.start()
+
+    info('*** Starting to execute commands\n')
+
+    info('set up master nginx')
+    lb.cmd("service keepalived start -D && nginx")
+    info(lb.cmd("nginx -t") + "\n")
+    info(lb.cmd("keepalived -t -f /etc/keepalived/keepalived.conf") + "\n")
+
+    info('set up backup nginx')
+    lb_backup.cmd("service keepalived start -D && nginx")
+    info(lb_backup.cmd("nginx -t") + "\n")
+    info(lb_backup.cmd("keepalived -t -f /etc/keepalived/keepalived.conf") + "\n")
+
+    info('Execute: client.cmd("time curl 10.0.0.1")\n')
+    info(client.cmd("time curl 10.0.0.1") + "\n")
+
+    info('Execute: client.cmd("time curl 10.0.0.1/hello/42")\n')
+    info(client.cmd("time curl 10.0.0.1/hello/42") + "\n")
+
+    data = {
+        "net": net,
+        "client": client, 
+        "server": server, 
+        "server2": server2,
+        "server3": server3,
+        "lb": lb, 
+        "lb_backup": lb_backup
+    }
+    return data;
--- demo.py
+++ demo.py
@@ -1,74 +1,19 @@
 #!/usr/bin/python
-"""
-This is an example how to simulate a client server environment.
-"""
-from mininet.net import Containernet
-from mininet.node import Controller
-from mininet.cli import CLI
-from mininet.link import TCLink
+
 from mininet.log import info, setLogLevel
+from mininet.cli import CLI
+
 import time
 from flask import Flask
+import containernet_service as containernetService
 
 setLogLevel('info')
+app = Flask(__name__)
 
-net = Containernet(controller=Controller)
-net.addController('c0')
+data = containernetService.initContainernet()
 
-info('*** Adding server and client container\n')
-server = net.addDocker('server', ip='10.0.1.1', dcmd="python app.py", dimage="server:latest")
-server2 = net.addDocker('server2', ip='10.0.1.2', dcmd="python app.py", dimage="server:latest")
-server3 = net.addDocker('server3', ip='10.0.1.3', dcmd="python app.py", dimage="server:latest")
-
-client = net.addDocker('client', ip='10.0.2.1', dimage="client:latest")
-lb = net.addDocker(
-    'lb', ip='10.0.0.2', dimage="nginx:latest", 
-    publish_all_ports= True,
-    ports=[80], port_bindings={80:80}
-)
-
-lb_backup = net.addDocker(
-    'lb_backup', ip='10.0.0.3', dimage="nginx_backup:latest", 
-    #publish_all_ports= True,
-    #ports=[80], port_bindings={81:80}
-)
-
-info('*** Setup network\n')
-s1 = net.addSwitch('s1')
-s2 = net.addSwitch('s2')
-s3 = net.addSwitch('s3')
-
-net.addLink(server,s1)
-net.addLink(server2,s1)
-net.addLink(server3,s1)
-
-net.addLink(lb,s2)
-net.addLink(lb_backup,s2)
-
-net.addLink(client,s3)
-
-net.addLink(s1, s2, cls=TCLink, delay='10ms', bw=1)
-net.addLink(s2, s3, cls=TCLink, delay='1ms', bw=1)
-net.start()
-
-info('*** Starting to execute commands\n')
-
-info('set up master nginx')
-lb.cmd("service keepalived start -D && nginx")
-info(lb.cmd("nginx -t") + "\n")
-info(lb.cmd("keepalived -t -f /etc/keepalived/keepalived.conf") + "\n")
-
-info('set up backup nginx')
-lb_backup.cmd("service keepalived start -D && nginx")
-info(lb_backup.cmd("nginx -t") + "\n")
-info(lb_backup.cmd("keepalived -t -f /etc/keepalived/keepalived.conf") + "\n")
-
-info('Execute: client.cmd("time curl 10.0.0.1")\n')
-info(client.cmd("time curl 10.0.0.1") + "\n")
-
-info('Execute: client.cmd("time curl 10.0.0.1/hello/42")\n')
-info(client.cmd("time curl 10.0.0.1/hello/42") + "\n")
-
-CLI(net)
-
-net.stop()
+if __name__ == "__main__":
+    net = data["net"]
+    CLI(net)
+    net.stop()
+    app.run(host='0.0.0.0', port=1234)
