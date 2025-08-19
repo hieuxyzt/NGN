45a3ef89536f90995fd0975f3e2085b1465ffd50
hieuxyzt <hieu650063@gmail.com>
2025-08-19 15:42:27 +0700

    update: get data from prometheus

--- scaling_server/src/main/java/ngn/ScalingService.java
+++ scaling_server/src/main/java/ngn/ScalingService.java
@@ -3,30 +3,42 @@ package ngn;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 @Slf4j
 public class ScalingService {
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final OkHttpClient okHttpClient = new OkHttpClient();
 
     @Scheduled(fixedDelayString = "10000", initialDelay = 0)
     public void scaleUp() {
-        JsonNode result = queryPrometheus("up");
-        log.info(result.toString());
+        long requestPerSecond = 0;
+
+        String query = "rate(http_client_requests_seconds_count{method='POST'}[5m])";
+        JsonNode result = queryPrometheus(query);
+        log.info("Clear Result: {}", result.toString());
+        for(JsonNode node : result) {
+            requestPerSecond += node.get("value").get(1).asLong();
+        }
+        log.info("Result: {}", requestPerSecond);
     }
 
     public JsonNode queryPrometheus(String query) {
         String prometheusUrl = "http://localhost:9090/api/v1/query";
         String url = prometheusUrl + "?query=" + query;
-        String jsonResponse = restTemplate.getForObject(url, String.class);
-        log.info(jsonResponse);
+        Request requestBuilder = new Request.Builder()
+                .url(UriComponentsBuilder.fromUriString(url).toUriString())
+                .header("Accept", "application/json")
+                .build();
 
-        try {
+        try (Response response = okHttpClient.newCall(requestBuilder).execute()){
+            String jsonResponse = response.body().string();
             ObjectMapper objectMapper = new ObjectMapper();
-            log.info(objectMapper.readTree(jsonResponse).get("data").toString());
             return objectMapper.readTree(jsonResponse).get("data").get("result");
         } catch (Exception e) {
             throw new RuntimeException("Error parsing Prometheus response", e);
